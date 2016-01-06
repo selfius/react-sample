bed349e81aa0bc30830ceb6d8afb3b9d9e26c6d3
Andrey Sviridov <self.polaris@gmail.com>
2016-01-06 15:24:10 +0300

    editable cell focus issue fixed

--- component/App.jsx
+++ component/App.jsx
@@ -63,21 +63,32 @@ class EditableCell extends React.Component {
     this.setState({value : event.target.value})
   }
 
-  render() {
-    let focusHandler = (input) => {
-      if (input && this.props.editable){
-        input.focus();
-      }
+  componentDidMount() {
+    this.updateFocus(this.refs.inputInternalRef)
+  }
+
+  componentDidUpdate(){
+    this.updateFocus(this.refs.inputInternalRef)
+  }
+
+  updateFocus(input) {
+    if (input && this.props.editable && (document.activeElement !== this.refs.inputInternalRef)){
+      input.setSelectionRange(9999,9999) //should be enough :)
+      input.focus()
     }
+  }
+
+  render() {
     var value = this.state.value;
     return <td key={this.props.key} onClick={this.props.clickHandler}>
 
     {this.props.editable ?
-      <input ref={focusHandler.bind(this)} type='text' onChange={this.onChange.bind(this)} value={value}/> :
+      <input ref='inputInternalRef' type='text' onChange={this.onChange.bind(this)} value={value}/> :
       <span>{this.state.value}</span>}
     </td>
 
   }
 }
+
 
 ReactDOM.render(<App/>, document.getElementById("rootContainer"))
